871a5dc448f473938b3dbe21d5a9ba48264bf82c
ripalkumar <patelripalacc@gmail.com>
2023-01-02 14:24:36 -0500

    complited

--- app.js
+++ app.js
@@ -4,7 +4,41 @@ console.log(
 );
 
 // Exercise 1
+let firstName = "Ripalkumar";
+let lastName = "Patel";
+var age = 34
 
 // Exercise 2
+firstName = "ripalkumar";
+lastName = "patel";
+age = 35;
+
+console.log(`My Name is ${firstName} ${lastName}.I am ${age} year old.`)
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = "true";
+
+console.log(`${language} was first release on year ${createdYear}.`)
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`the Track-pant in size ${selectedSize} is $${price} and has a ${salePercentage}% sale with ${inStock}. `)
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`The book "${title}" by "${author}" has total page ${pageCount} and i am at ${bookmark} page.`
+)
+ 
